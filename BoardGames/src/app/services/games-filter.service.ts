7109a517c21d1e420cc9945e6d8841533765feae
mateuszdobrakowski <mateusz.dobrakowski@avanade.com>
2018-12-14 18:08:02 +0100

    unsaved file

--- BoardGames/src/app/services/games-filter.service.ts
+++ BoardGames/src/app/services/games-filter.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
+import { GamesService } from './games.service';
+import { Game } from '../game';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GameFilterService {
 
-  constructor() { }
+  gamesList: Game[];
+  filteredGamesList: Game[];
+
+
+  constructor(private gamesService: GamesService) { 
+
+    this.gamesService.getGames()
+    .subscribe(
+      (gamesList: Game[]) => this.filteredGamesList = this.gamesList = gamesList
+    );
+  }
+
+  search(phrase:string) {
+    this.filteredGamesList = this.gamesList.filter(x => x.name.toLowerCase().includes(phrase.toLowerCase()));
+    console.log(phrase);
+  }
+
+  getFilteredGames(): Observable <Game[]> {
+    return this.filteredGamesList
+  }
+
 }
